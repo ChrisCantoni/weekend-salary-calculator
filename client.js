dee6b32d550ec65da981721c9d361265e2942d13
Chris Cantoni <chris.cantoni@gmail.com>
2023-09-16 15:02:09 -0500

    Add function to append first name to DOM.

--- client.js
+++ client.js
@@ -1,5 +1,13 @@
 console.log('linked')
 
+let employeeTable = document.querySelector('#employee-info');
+
 function submitEmployee(event){
-    
+    event.preventDefault();
+    let firstName = document.querySelector('#first-name').value;
+    employeeTable.innerHTML +=`
+    <tr>
+    <td>${firstName}</td>
+    </tr>`
+    console.log('we ran the code')
 }
