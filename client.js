c32cdc0a1154764be05d234bd8fd06e0562425e2
Chris Cantoni <chris.cantoni@gmail.com>
2023-09-16 15:48:51 -0500

    Add input fields and DOM append  for last name, ID, title.

--- client.js
+++ client.js
@@ -3,21 +3,44 @@ console.log('linked')
 let employeeTable = document.querySelector('#employee-info');
 let totalSalary = 0;
 let totalSalaryAmount = document.querySelector('#total-salary');
+let employeeList = [];
 
+// Adds all employees to an array
+function addEmployee(firstName, lastName, idNumber, title, salary) {
+    let employeeToAdd = {
+        firstName,
+        lastName,
+        idNumber,
+        title,
+        salary
+    }
+    employeeList.push(employeeToAdd);
+    return employeeToAdd;
+}
+
+// Takes in all info on employee, adds to DOM and array
 function submitEmployee(event){
     event.preventDefault();
     let firstName = document.querySelector('#first-name').value;
+    let lastName = document.querySelector('#last-name').value;
+    let idNumber = document.querySelector('#id-number').value;
+    let title = document.querySelector('#job-title').value;
     let salary = Number(document.querySelector('#annual-salary').value);
+    addEmployee(firstName, lastName, idNumber, title, salary);
     totalSalary = salary + totalSalary;
     let totalMonthly = totalSalary / 12;
     console.log(totalSalary);
     employeeTable.innerHTML +=`
-    <tr>
-    <td>${firstName}</td>
-    <td>$${salary}</td>
-    </tr>`
+        <tr>
+            <td>${firstName}</td>
+            <td>${lastName}</td>
+            <td>${idNumber}</td>
+            <td>${title}</td>
+            <td>$${salary}</td>
+        </tr>`
     console.log(totalMonthly);
     totalSalaryAmount.innerHTML = `${totalMonthly}`;
+    console.log(employeeList);
 }
 
 
