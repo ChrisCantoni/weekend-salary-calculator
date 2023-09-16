95167bc8808c26ff7eb798ca20f03ce190d08a0a
Chris Cantoni <chris.cantoni@gmail.com>
2023-09-16 15:07:47 -0500

    Add input for salary and append to DOM.

--- client.js
+++ client.js
@@ -5,9 +5,11 @@ let employeeTable = document.querySelector('#employee-info');
 function submitEmployee(event){
     event.preventDefault();
     let firstName = document.querySelector('#first-name').value;
+    let salary = document.querySelector('#annual-salary').value;
     employeeTable.innerHTML +=`
     <tr>
     <td>${firstName}</td>
+    <td>$${salary}</td>
     </tr>`
     console.log('we ran the code')
 }
