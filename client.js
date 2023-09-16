2b8aaf7b3d10661cecd492cafc3ba480143fbf8b
Chris Cantoni <chris.cantoni@gmail.com>
2023-09-16 15:19:54 -0500

    Convert salary input to number, added to total.

--- client.js
+++ client.js
@@ -1,15 +1,23 @@
 console.log('linked')
 
 let employeeTable = document.querySelector('#employee-info');
+let totalSalary = 0;
+let totalSalaryAmount = document.querySelector('#total-salary');
 
 function submitEmployee(event){
     event.preventDefault();
     let firstName = document.querySelector('#first-name').value;
-    let salary = document.querySelector('#annual-salary').value;
+    let salary = Number(document.querySelector('#annual-salary').value);
+    totalSalary = salary + totalSalary;
+    console.log(totalSalary);
     employeeTable.innerHTML +=`
     <tr>
     <td>${firstName}</td>
     <td>$${salary}</td>
     </tr>`
-    console.log('we ran the code')
+    console.log(totalSalary);
+    totalSalaryAmount.innerHTML = `${totalSalary}`;
 }
+
+
+
