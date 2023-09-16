61733b6d35059472a7c7d19dcfc7c15cb2599e17
Chris Cantoni <chris.cantoni@gmail.com>
2023-09-16 15:21:49 -0500

    Convert total salary to monthly amount on DOM.

--- client.js
+++ client.js
@@ -9,14 +9,15 @@ function submitEmployee(event){
     let firstName = document.querySelector('#first-name').value;
     let salary = Number(document.querySelector('#annual-salary').value);
     totalSalary = salary + totalSalary;
+    let totalMonthly = totalSalary / 12;
     console.log(totalSalary);
     employeeTable.innerHTML +=`
     <tr>
     <td>${firstName}</td>
     <td>$${salary}</td>
     </tr>`
-    console.log(totalSalary);
-    totalSalaryAmount.innerHTML = `${totalSalary}`;
+    console.log(totalMonthly);
+    totalSalaryAmount.innerHTML = `${totalMonthly}`;
 }
 
 
